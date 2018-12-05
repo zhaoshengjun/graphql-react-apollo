df3952e17ebaa686af73bf4c92dee94f1d878c14
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-05 22:00:12 +1000

    extract query

--- src/Recipes.js
+++ src/Recipes.js
@@ -2,18 +2,18 @@ import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 
+const recipesQuery = gql`
+  {
+    recipes {
+      id
+      title
+    }
+  }
+`;
+
 export default function() {
   return (
-    <Query
-      query={gql`
-        {
-          recipes {
-            id
-            title
-          }
-        }
-      `}
-    >
+    <Query query={recipesQuery}>
       {({ data, loading, error }) => {
         if (loading) return <p>Loading...</p>;
         if (error) return <p>Something went wrong</p>;
