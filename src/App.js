cf614b4d0d045f412bffa3b0cf2fdf746a1a7ec5
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-05 21:42:28 +1000

    ApolloProvider and ApolloConsumer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
+import { ApolloProvider, ApolloConsumer } from "react-apollo";
 import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000"
 });
 
-client
-  .query({
-    query: gql`
-      {
-        recipes {
-          id
-          title
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello World</div>;
+        <ApolloConsumer>
+          {client => {
+            client
+              .query({
+                query: gql`
+                  {
+                    recipes {
+                      id
+                      title
+                    }
+                  }
+                `
+              })
+              .then(result => console.log(result));
+            return null;
+          }}
+        </ApolloConsumer>
+      </ApolloProvider>
+    );
   }
 }
 
