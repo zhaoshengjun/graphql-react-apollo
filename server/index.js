53db1a491a1d4a9453013b3ed4c1a76902f08982
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-05 22:34:32 +1000

    update server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const recipes = [
-  { id: 1, title: "Yammy Pumpkin" },
-  { id: 2, title: "Egg Roll" }
+  { id: 1, title: "Yammy Pumpkin", vegetarian: true },
+  { id: 2, title: "Egg Roll", vegetarian: false }
 ];
 
 const typeDefs = gql`
   type Recipe {
     id: Int
     title: String
+    vegetarian: Boolean
   }
   type Query {
-    recipes: [Recipe]
+    recipes(vegetarian: Boolean): [Recipe]
   }
 `;
 const resolvers = {
   Query: {
-    recipes: () => recipes
+    recipes: (parent, args, context, info) => {
+      const { vegetarian } = args;
+      if (vegetarian === undefined) {
+        return recipes;
+      } else {
+        return recipes.filter(recipe => recipe.vegetarian === vegetarian);
+      }
+    }
   }
 };
 
