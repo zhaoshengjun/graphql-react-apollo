23a91b8016c01be3f01602cfa9dcbfb8d8a23733
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-05 21:26:01 +1000

    use apollo client

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000"
+});
+
+client
+  .query({
+    query: gql`
+      {
+        recipes {
+          id
+          title
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>Hello World</div>;
   }
 }
 
