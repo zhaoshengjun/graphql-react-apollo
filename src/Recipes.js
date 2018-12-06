6b00d0d483c304a72ccc9c8e2089b68ad50d2d0a
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-06 19:28:23 +1000

    use variable in query

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 
 const recipesQuery = gql`
-  {
-    recipes {
+  query recipes($vegetarian: Boolean!) {
+    recipes(vegetarian: $vegetarian) {
       id
       title
     }
   }
 `;
 
-export default function() {
-  return (
-    <Query query={recipesQuery}>
-      {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Something went wrong</p>;
-        if (data.recipes === undefined) return null;
-        return (
-          <ul>
-            {data.recipes.map(({ id, title }) => (
-              <li key={id}>{title}</li>
-            ))}
-          </ul>
-        );
-      }}
-    </Query>
-  );
+export default class Recipes extends Component {
+  state = {
+    vegetarian: false
+  };
+
+  updateVegetarian = ({ target: { checked } }) => {
+    this.setState({ vegetarian: checked });
+  };
+
+  render() {
+    const { vegetarian } = this.state;
+    return (
+      <Fragment>
+        <label>
+          <input
+            type="checkbox"
+            checked={this.state.vegetarian}
+            onChange={this.updateVegetarian}
+          />
+          <span>Vegetarian</span>
+        </label>
+        <Query query={recipesQuery} variables={{ vegetarian }}>
+          {({ data, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Something went wrong</p>;
+            if (data.recipes === undefined) return null;
+            return (
+              <ul>
+                {data.recipes.map(({ id, title }) => (
+                  <li key={id}>{title}</li>
+                ))}
+              </ul>
+            );
+          }}
+        </Query>
+      </Fragment>
+    );
+  }
 }
