4155e39a4ed9eae8db1b9e4937d8b9be6d30fc94
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-07 22:16:46 +1000

    refetch

--- src/Recipes.js
+++ src/Recipes.js
@@ -31,48 +31,54 @@ export default class Recipes extends Component {
           <span>Vegetarian</span>
         </label>
         <Query query={recipesQuery} variables={{ vegetarian }}>
-          {({ data, loading, error }) => {
+          {({ data, loading, error, refetch }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Something went wrong</p>;
             if (data.recipes === undefined) return null;
             return (
-              <ul>
-                {data.recipes.map(({ id, title, isStarred }) => (
-                  <li key={id}>
-                    {title}
-                    <Mutation
-                      mutation={updateRecipeStarredMutation}
-                      refetchQueries={[
-                        {
-                          query: recipesQuery,
-                          variables: { vegetarian: false }
-                        },
-                        { query: recipesQuery, variables: { vegetarian: true } }
-                      ]}
-                      awaitRefetchQueries={true}
-                    >
-                      {(updateRecipeStarred, { loading, error }) => (
-                        <button
-                          onClick={() =>
-                            updateRecipeStarred({
-                              variables: { id, isStarred: !isStarred }
-                            })
+              <Fragment>
+                <ul>
+                  {data.recipes.map(({ id, title, isStarred }) => (
+                    <li key={id}>
+                      {title}
+                      <Mutation
+                        mutation={updateRecipeStarredMutation}
+                        refetchQueries={[
+                          {
+                            query: recipesQuery,
+                            variables: { vegetarian: false }
+                          },
+                          {
+                            query: recipesQuery,
+                            variables: { vegetarian: true }
                           }
-                          className="star-btn"
-                          style={{
-                            color: isStarred ? "orange" : "grey",
-                            animation: loading
-                              ? "inflate 0.7s ease infinite alternate"
-                              : "none"
-                          }}
-                        >
-                          ★ {error && "Failed to update"}
-                        </button>
-                      )}
-                    </Mutation>
-                  </li>
-                ))}
-              </ul>
+                        ]}
+                        awaitRefetchQueries={true}
+                      >
+                        {(updateRecipeStarred, { loading, error }) => (
+                          <button
+                            onClick={() =>
+                              updateRecipeStarred({
+                                variables: { id, isStarred: !isStarred }
+                              })
+                            }
+                            className="star-btn"
+                            style={{
+                              color: isStarred ? "orange" : "grey",
+                              animation: loading
+                                ? "inflate 0.7s ease infinite alternate"
+                                : "none"
+                            }}
+                          >
+                            ★ {error && "Failed to update"}
+                          </button>
+                        )}
+                      </Mutation>
+                    </li>
+                  ))}
+                </ul>
+                <button onClick={() => refetch()}>Refresh</button>
+              </Fragment>
             );
           }}
         </Query>
